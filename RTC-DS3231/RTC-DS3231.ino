72a5a3e50fa51dcd436ed9f2fead3e22dfaa2508
David Wang <whcdavidca@gmail.com>
2017-04-15 22:28:48 -0400

    Added Alarm Clock Function: Ring for one minute\nAdded two more button to set alarm time\nAdded buzzer for Alarm\n\n--David Wang

--- RTC-DS3231/RTC-DS3231.ino
+++ RTC-DS3231/RTC-DS3231.ino
@@ -1,14 +1,15 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
 #include <LiquidCrystal.h>
 #include "RTClib.h"
 #include <stdio.h>
 
-//namespace {
-  
 char daysOfTheWeek[7][12] = {"Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"};
 uint8_t menu_button = 9;
-
+uint8_t increase_button = 8;
+uint8_t next_button=7;
+uint8_t buzzer_pin=6;
+int cSec,cMin,cHour;
 RTC_DS3231 rtc;
 
 DateTime now;
@@ -18,38 +19,7 @@ int menu = 0; // menu selector. 0 = display time, 1 = alarm menu
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-//String dayToString(Time::Day day){
-//  switch(day){
-//    case 1: return "Sun";
-//    case 2: return "Monday";
-//    case 3: return "Tuesday";
-//    case 4: return "Wednesday";
-//    case 5: return "Thursday";
-//    case 6: return "Friday";
-//    case 7: return "Sat";
-//  }
-//  return "uknown";
-//
-//  
-//}
 void print_time(){
-
-//  Time t = rtc.time();
-//  Serial.print(t.yr);Serial.print(" ");
-//  Serial.print(t.mon);Serial.print(" ");
-//  Serial.print(t.date);Serial.print(" ");
-//  Serial.print(t.hr);Serial.print(" ");
-//  Serial.print(t.min);Serial.print(" ");
-//  Serial.print(t.sec);Serial.print(" ");
-//  Serial.println();
-
-//  String day = dayToString(t.day);
-//  char buf[50];
-//  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d \n %02d:%02d:%02d",
-//             day.c_str(),
-//             t.yr, t.mon, t.date,
-//             t.hr, t.min, t.sec);
-//  Serial.println(buf);
   lcd.setCursor(0,0);
   lcd.print(now.year(), DEC);
   lcd.print("-");
@@ -67,26 +37,75 @@ void print_time(){
   lcd.print("   ");
 
   }
-//}
 
 void print_alarm(){
   lcd.setCursor(0,0);
   lcd.print("ALARM MENU");
+  lcd.setCursor(0,1);
+  
+
+  int currentPos=0;
+  lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("ALARM MENU");
+      lcd.setCursor(0,1);
+      lcd.print(cHour);
+      lcd.print(":");
+      lcd.print(cMin);
+      lcd.print(":");
+      lcd.print(cSec);
+  while(true){
+    if(button_press(increase_button)){
+      switch(currentPos){
+        case 0:
+          cSec=(cSec+1)%60;
+          break;
+        case 1:
+          cMin=(cMin+1)%60;
+          break;
+        case 2:
+          cHour=(cHour+1)%24;
+          break;
+      }
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("ALARM MENU");
+      lcd.setCursor(0,1);
+      lcd.print(cHour);
+      lcd.print(":");
+      lcd.print(cMin);
+      lcd.print(":");
+      lcd.print(cSec);
+      delay(300);
+    }
+    if(button_press(next_button)){
+      currentPos=(currentPos+1)%3;
+      delay(300);
+    }
+    if(button_press(menu_button)){
+      menu=0;
+      delay(300);
+      return;
+      
+    }
+    
+    
+  }
+}
+bool button_press(uint8_t currentButton){
+  if(digitalRead(currentButton) == HIGH)
+    return false;
+  delay(300);
+  return true;
 }
 
-bool menu_button_press(){
-  if (digitalRead(menu_button) == HIGH){
-    return false;
-  }
-  else{
-    delay(300); //wait for user to relasee button
-    return true;
-  }
-}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  cSec=0;
+  cMin=10;
+  cHour=22;
 //  rtc.writeProtect(false);
 //  rtc.halt(false);
   delay(3000); // wait for console opening
@@ -95,7 +114,7 @@ void setup() {
     Serial.println("Couldn't find RTC");
     while (1);
   }
-  
+
   if (rtc.lostPower()) {
     Serial.println("RTC lost power, lets set the time!");
     // following line sets the RTC to the date & time this sketch was compiled
@@ -104,7 +123,7 @@ void setup() {
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
-//  
+//
 //  Time t (2017, 4, 14, 22, 1, 23, Time::kFriday);
 //  Time t=rtc.time();
 //  rtc.time(t);
@@ -113,23 +132,27 @@ void setup() {
   lcd.begin(16, 2);
 
   pinMode(menu_button, INPUT);
-  
- 
+  pinMode(increase_button, INPUT);
+  pinMode(next_button, INPUT);
+  pinMode(buzzer_pin, OUTPUT);
 }
 
 
 
 void loop() {
   now = rtc.now();
-  if (menu_button_press()){
-    
-
+  noTone(buzzer_pin);
+  if(now.hour()==cHour&&now.minute()==cMin){
+    tone(buzzer_pin,3830);
+    delay(300);
+  }
+  if (button_press(menu_button)){
     switch(menu){
        case 0:
           lcd.clear();
           menu = 1;
           break;
-       
+
        case 1:
           lcd.clear();
           menu = 0;
@@ -137,18 +160,18 @@ void loop() {
 
        default:
           lcd.clear();
-          menu = 1;       
+          menu = 1;
     }
-    
+
   }
-  
+
   switch(menu){
-    
+
     case 0:
       print_time();
       delay(200);
       break;
-      
+
     case 1:
       print_alarm();
       delay(200);
@@ -156,8 +179,9 @@ void loop() {
 
     default:
       print_time();
-      delay(200);  
+      delay(200);
   }
-  
+
 
 }
+
